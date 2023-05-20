563cfc99327cce713e03a978ac4e2618d4885946
francisco neto <francisco_carneiro@hotmail.com.br>
2023-05-20 20:41:25 -0300

    primeiro commit

---
+++ edena/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name="url_index"),
+]
